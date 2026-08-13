cdf4d91a8b1a2764daf4cfdaadfaadf634ff4e7a
Hanan Loulou <H.Loulou@campus.lmu.de>
2026-08-13 14:24:07 +0200

    adding a new function

---
+++ munich/R/my_sum.R
@@ -0,0 +1,18 @@
+#' My Summation Function
+#'
+#' This is my first function and it sums two numbers.
+#'
+#' @param x first summand.
+#' @param y second summand.
+#'
+#' @return The sum of `x` and `y`.
+#' @export
+#'
+#' @note This function is a bit boring but that is ok.
+#' @seealso [Arithmetic] for an easier way.
+#'
+#' @examples
+#' my_sum(1, 2)
+my_sum <- function(x, y) {
+  x + y
+}
